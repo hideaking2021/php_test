e8495648ba5074e9e752893e217d4fec6f680958
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-02-18 22:57:57 +0900

    背景変更

--- work/web/result.php
+++ work/web/result.php
@@ -2,16 +2,15 @@
 
 require('../app/functions.php');
 
-// $color = filter_input(INPUT_GET, 'color');
-// $color = isset($color) ? $color : 'None selected';
-// $color = $color ?? 'None selected';
-$color = filter_input(INPUT_GET, 'color') ?? 'None selected';
+
+$colorFromGet = filter_input(INPUT_GET, 'color') ?? 'transparent';
+setcookie('color' , $colorFromGet);
 
 include('../app/_parts/_header.php');
 
 ?>
 
-<p><?= h($color); ?></p>
+<p><?= h($colorFromGet); ?></p>
 <p><a href="index.php">Go back</a></p>
 
 <?php
