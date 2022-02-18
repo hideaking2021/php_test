6372413900abf7a478918b5eb991daaf899431f9
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-02-18 23:32:55 +0900

    get リセット

--- work/web/index.php
+++ work/web/index.php
@@ -3,18 +3,15 @@
 require('../app/functions.php');
 
 
-
 include('../app/_parts/_header.php');
 
 ?>
 
 
-<form action="result.php" method="get"> 
-  <label><input type="radio" name="color" value="orange">Orange</label>
-  <label><input type="radio" name="color" value="pink">Pink</label>
-  <label><input type="radio" name="color" value="gold">Gold</label>
+<form action="result.php" method="post"> 
+  
   <button>Send</button>
-  <a href="reset.php">[reset]</a>
+  
 </form>
 
 <?php
