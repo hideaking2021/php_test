044bd6555a612c92be64d8c8dcf02881b9ccc021
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-02-18 22:10:26 +0900

    チェックボックス

--- work/web/index.php
+++ work/web/index.php
@@ -1,18 +1,22 @@
 <?php
 
-  
+require('../app/functions.php');
 
-  $name = 'Taro <script>alert(1);</script>';
+include('../app/_parts/_header.php');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>PHP Practice</title>
-</head>
-<body>
-  <p>Hello, <?= h($name) ?>!</p>
-</body>
-</html>
+
+<form action="result.php" method="get"> 
+  <label><input type="checkbox" name="colors[]" value="orange">Orange</label>
+  <label><input type="checkbox" name="colors[]" value="pink">Pink</label>
+  <label><input type="checkbox" name="colors[]" value="gold">Gold</label>
+  <button>Send</button>
+</form>
+
+<?php
+
+
+
+include('../app/_parts/_footer.php');
+
